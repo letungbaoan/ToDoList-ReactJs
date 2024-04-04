e28fb988e135d052dcbd6f29d95cc6b568c6b016
letungbaoan <baoan7604@gmail.com>
2024-04-04 15:09:23 +0700

    Create components

---
+++ src/components/ToDoList/TodoList.tsx
@@ -0,0 +1,15 @@
+import TaskInput from '../TaskInput'
+import TaskList from '../TaskList'
+import styles from './todoList.module.scss'
+
+export default function TodoList() {
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.todoListContainer}>
+        <TaskInput />
+        <TaskList doneTaskList={false} />
+        <TaskList doneTaskList />
+      </div>
+    </div>
+  )
+}
