1ed7d162bdadeefa84ca5ca70b4ba9ba74a48df7
letungbaoan <baoan7604@gmail.com>
2024-04-05 22:00:47 +0700

    Add Add new task funtion

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,31 +1,40 @@
+import React from 'react'
+import { Todo } from '../../@types/todo.type'
 import styles from './taskList.module.scss'
 
 interface TaskListProps {
-  doneTaskList: boolean
+  doneTaskList?: boolean
+  todos: Todo[]
+  handleDoneTodo: (id: string, done: boolean) => void
 }
 
 export default function TaskList(props: TaskListProps) {
-  const { doneTaskList } = props
+  const { doneTaskList, todos, handleDoneTodo } = props
+
+  const onChangeCheckbox = (idTodo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.target
+    handleDoneTodo(idTodo, checked)
+  }
+
   return (
     <div className='mb-2'>
       <h2 className={styles.title}>{doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}</h2>
       <div className={styles.tasks}>
-        <div className={styles.task}>
-          <input type='checkbox' className={styles.taskCheckbox} />
-          <span className={styles.taskName}>Học bài</span>
-          <div className={styles.taskActions}>
-            <button className={styles.taskBtn}>🖊️</button>
-            <button className={styles.taskBtn}>🗑️</button>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type='checkbox'
+              className={styles.taskCheckbox}
+              checked={todo.done}
+              onChange={onChangeCheckbox(todo.id)}
+            />
+            <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
+            <div className={styles.taskActions}>
+              <button className={styles.taskBtn}>🖊️</button>
+              <button className={styles.taskBtn}>🗑️</button>
+            </div>
           </div>
-        </div>
-        <div className={styles.task}>
-          <input type='checkbox' className={styles.taskCheckbox} />
-          <span className={styles.taskName}>Học bài</span>
-          <div className={styles.taskActions}>
-            <button className={styles.taskBtn}>🖊️</button>
-            <button className={styles.taskBtn}>🗑️</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
