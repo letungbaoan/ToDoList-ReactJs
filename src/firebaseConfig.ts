879ac505059af166587370c243bf8006dfb11eb5
letungbaoan <baoan7604@gmail.com>
2024-10-24 18:04:52 +0700

    intergrate Firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,14 +3,16 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAeEftV5MzE0H1XdoX35aNFBEN3CgQX_O0',
-  authDomain: 'todolist-2e2ea.firebaseapp.com',
-  projectId: 'todolist-2e2ea',
-  storageBucket: 'todolist-2e2ea.appspot.com',
-  messagingSenderId: '144253648699',
-  appId: '1:144253648699:web:9d3ba8e361e230b166c11e',
-  measurementId: 'G-8NWCTBE5CD'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
+
+console.log(firebaseConfig)
 
 const app = initializeApp(firebaseConfig)
 export const database = getFirestore(app)
